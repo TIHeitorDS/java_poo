3ceaaeb3964f4f23200dbc9860effe27cb1bc624
TIHeitorDS <heitorclaudinods@gmail.com>
2023-11-27 20:44:11 -0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    // inicializando o programa com o método main
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    } // fim do método main
+}
