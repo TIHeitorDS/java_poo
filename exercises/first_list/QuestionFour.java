3af2a372e166f19f1ce15826a993dded6841a617
TIHeitorDS <heitorclaudinods@gmail.com>
2023-11-30 18:01:54 -0300

    feat: fourth question

---
+++ exercises/first_list/QuestionFour.java
@@ -0,0 +1,21 @@
+package exercises.first_list;
+
+import java.util.Scanner;
+
+public class QuestionFour {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double temperature;
+        double tempInFahrenheit;
+
+        System.out.print("Informe a temperatura em Celsius: ");
+        temperature = input.nextDouble();
+
+        tempInFahrenheit = temperature * 1.8 + 32;
+
+        System.out.printf("%.2f°C = %.2f°F", temperature, tempInFahrenheit);
+
+        input.close();
+    }
+}
