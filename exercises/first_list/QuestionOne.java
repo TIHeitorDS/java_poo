9c31d5118dc9153852e2b59de59886f211505261
TIHeitorDS <heitorclaudinods@gmail.com>
2023-11-30 16:52:56 -0300

    feat: first question of exercise

---
+++ exercises/first_list/QuestionOne.java
@@ -0,0 +1,21 @@
+package exercises.first_list;
+
+import java.util.Scanner;
+
+public class QuestionOne {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        float numero;
+        float area;
+
+        System.out.print("Informe o valor da área do quadrado: ");
+        numero = input.nextFloat();
+
+        area = numero * numero;
+
+        System.out.printf("A área do quadrado é %.1f.", area);
+
+        input.close();
+    }
+}
