3b7625fc1ee933258376f974869b8b1ff12b969e
TIHeitorDS <heitorclaudinods@gmail.com>
2023-12-12 17:13:56 -0300

    feat: account program

---
+++ account/Account.java
@@ -0,0 +1,15 @@
+// Classe que contém uma variável de instância name
+// e métodos para atribuir e obter seu valor
+package account;
+
+public class Account {
+    private String name; // variável de instância
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ account/AccountTest.java
@@ -0,0 +1,22 @@
+// Cria e manipula um objeto Account
+package account;
+
+import java.util.Scanner;
+
+public class AccountTest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Account myAccount = new Account();
+
+        String name;
+
+        System.out.print("Please enter a name: ");
+        name = input.nextLine();
+
+        myAccount.setName(name);
+
+        System.out.printf("name in object Account is: %s\n", myAccount.getName());
+
+        input.close();
+    }
+}
