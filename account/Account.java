9bbb075818aa98e1da936cb4e73fe7574f84f5eb
TIHeitorDS <heitorclaudinods@gmail.com>
2023-12-16 18:30:43 -0300

    feat: account

--- account/Account.java
+++ account/Account.java
@@ -4,6 +4,27 @@ package account;
 
 public class Account {
     private String name; // variável de instância
+    private double balance; // variável de inst^ncia
+
+    // construtor que aceita dois parâmetros
+    public Account(String name, double balance) { // o nome do construtor é o nome da classe 
+        this.name = name;
+        if (balance > 0.0) {
+            this.balance = balance;
+        }
+    }
+
+    // método para depositar uma quantia válida
+    public void deposit(double depositAmount) {
+        if (depositAmount > 0.0) {
+            balance = balance + depositAmount;
+        }
+    }
+
+    // método para exibir o saldo da conta
+    public double getBalance() {
+        return balance;
+    }
 
     public void setName(String name) {
         this.name = name;
--- account/AccountTest.java
+++ account/AccountTest.java
@@ -6,18 +6,32 @@ import java.util.Scanner;
 public class AccountTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Account myAccount = new Account();
+        Account account1 = new Account("Heitor Claudino Dantas", 450);
+        Account account2 = new Account("Henzo G Claudino Dantas", -7.15);
 
-        System.out.println(myAccount.getName());
+        double depositAmount;
 
-        String name;
+        // exibindo o saldo inicial
+        System.out.printf("%s balance: %f\n", account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: %f\n", account2.getName(), account2.getBalance());
 
-        System.out.print("Please enter a name: ");
-        name = input.nextLine();
+        // solicitando o valor do deposito para a conta um
+        System.out.print("Enter deposit amount for account1: ");
+        depositAmount = input.nextDouble();
+        account1.deposit(depositAmount);
 
-        myAccount.setName(name);
+        // exibindo o novo saldo
+        System.out.printf("\n%s balance: %f", account1.getName(), account1.getBalance());
+        System.out.printf("\n%s balance: %f\n", account2.getName(), account2.getBalance());
 
-        System.out.printf("name in object Account is: %s\n", myAccount.getName());
+        // solicitando o valor do deposito para a conta dois
+        System.out.print("Enter deposit amount for account2: ");
+        depositAmount = input.nextDouble();
+        account2.deposit(depositAmount);
+        
+        // exibindo o novo saldo
+        System.out.printf("\n%s balance: %f", account1.getName(), account1.getBalance());
+        System.out.printf("\n%s balance: %f\n", account2.getName(), account2.getBalance());
 
         input.close();
     }
