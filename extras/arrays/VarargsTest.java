c4c6a42ad76c423de68d4a381578f3e8dd726dda
TIHeitorDS <heitorclaudinods@gmail.com>
2024-01-09 01:28:06 -0300

    feat: var args

---
+++ extras/arrays/VarargsTest.java
@@ -0,0 +1,25 @@
+package extras.arrays;
+
+public class VarargsTest {
+    public static double average(double... numbers) {
+        double total = 0;
+
+        for (double number : numbers) {
+            total += number;
+        }
+
+        return total / numbers.length;
+    }
+
+    public static void main(String[] args) {
+        double num1 = 10.0;
+        double num2 = 20.0;
+        double num3 = 30.0;
+        double num4 = 40.0;
+
+        System.out.println("Average of num1 and num2: " + average(num1, num2));
+        System.out.println("Average of num1, num2 and num3: " + average(num1, num2, num3));
+        System.out.println("Average of num1, num2, num3 and num4: " + average(num1, num2, num3, num4));
+
+    }
+}
