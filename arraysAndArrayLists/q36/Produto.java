35b43f7e6a69610d91875bff3b3182db10933055
TIHeitorDS <heitorclaudinods@gmail.com>
2024-01-11 08:38:41 -0300

    feat: question 36

---
+++ arraysAndArrayLists/q36/Produto.java
@@ -0,0 +1,13 @@
+package arraysAndArrayLists.q36;
+
+public class Produto {
+    public int calculaProduto(int... numeros) {
+        int total = 1;
+
+        for (int numero : numeros) {
+            total *= numero;
+        }
+
+        return total;
+    }
+}
