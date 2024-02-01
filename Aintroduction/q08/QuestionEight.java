d2b8e76eab49247b658db639fc3bce252f4cdcba
TIHeitorDS <heitorclaudinods@gmail.com>
2024-02-01 20:01:06 -0300

    fix: output

--- Aintroduction/q08/QuestionEight.java
+++ Aintroduction/q08/QuestionEight.java
@@ -9,41 +9,38 @@ public class QuestionEight {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        final int eletros = 222, cama = 111;
         int code;
-        double price, finalValue;
+        double price;
 
         System.out.print("Informe o código do setor: ");
         code = input.nextInt();
-
         System.out.print("Informe o preço do produto: ");
         price = input.nextDouble();
+        
+        input.close();
 
         switch(code) {
-            case eletros:
+            case 222:
+                System.out.println("Nome do setor: Eletros.");
                 if (price > 500) {
-                    finalValue = price - (price * (0.1));
-                } else {
-                    finalValue = price;
+                 price *= 0.90;
                 }
-
-                System.err.printf("\nNome do setor: Eletros\nValor do produto com desconto: R$%.2f.\n", finalValue);
                 break;
-            case cama:
+            case 111:
+                System.out.println("Nome do setor: Cama, Mesa, Banho.");
                 if (price > 100) {
-                    finalValue = price - (price * (0.4));
-                } else if (price >= 50 && price <= 100) {
-                    finalValue = price - (price * (0.2));
+                    price *= 0.60;
+                } else if (price >= 50) {
+                    price *= 0.80;
                 } else {
-                    finalValue = price - (price * (0.1));
+                    price *= 0.90;
                 }
-
-                System.err.printf("\nNome do setor: Cama\nValor do produto com desconto: R$%.2f\n.", finalValue);
                 break;
             default:
-                System.err.println("Setor inválido.");
+                System.out.println("Setor inválido.");
+                return;
         }
 
-        input.close();
+        System.out.println("Preço do produto com desconto: " + price);
     }
 }
