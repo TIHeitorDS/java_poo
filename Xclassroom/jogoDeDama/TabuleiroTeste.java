f22deb459b7acff33283e588ccc35f3af6452a34
TIHeitorDS <heitorclaudinods@gmail.com>
2024-02-05 17:43:59 -0300

    arrays

---
+++ Xclassroom/jogoDeDama/TabuleiroTeste.java
@@ -0,0 +1,9 @@
+package Xclassroom.jogoDeDama;
+
+public class TabuleiroTeste {
+    public static void main(String[] args) {
+        Tabuleiro tabuleiro = new Tabuleiro();
+
+        System.out.println(tabuleiro.toString());
+    }
+}
