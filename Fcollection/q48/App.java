416f0045ff035c25559d6a51b1c3e12ede52ef7d
TIHeitorDS <heitorclaudinods@gmail.com>
2024-03-24 09:44:33 -0300

    refactor: simplify question 48

--- Fcollection/q48/App.java
+++ Fcollection/q48/App.java
@@ -6,37 +6,20 @@ import java.util.ListIterator;
 
 public class App {
     public static void main(String[] args) {
-        LinkedList<Character> characters = new LinkedList<>(
+        LinkedList<Character> list = new LinkedList<>(
                 Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'));
-        LinkedList<Character> charactersCopy;
-
-        charactersCopy = inverse(characters);
-
-        System.out.println("list original: ");
-
-        for (Character character : characters) {
-            System.out.print(character + " ");
-        }
-
-        System.out.println("\n\nlist invertida: ");
-
-        for (Character character : charactersCopy) {
-            System.out.print(character + " ");
-        }
-    }
-
-    public static LinkedList<Character> inverse(LinkedList<Character> list) {
+        LinkedList<Character> invertedList = new LinkedList<>();
         ListIterator<Character> listIterator = list.listIterator();
-        LinkedList<Character> copy = new LinkedList<>();
 
         while(listIterator.hasNext()) {
             listIterator.next();
         }
 
-        while (listIterator.hasPrevious()) {
-            copy.add(listIterator.previous());
+        while(listIterator.hasPrevious()) {
+            invertedList.add(listIterator.previous());
         }
 
-        return copy;
+        System.out.println("Lista original: " + list);
+        System.out.println("lista invertida: " + invertedList);
     }
 }
